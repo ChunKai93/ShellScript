d47e41fa3a468af600d2e6811471e282ef8058d7
ChunKai,Chau <33021745+ChunKai93@users.noreply.github.com>
2020-03-31 17:17:11 +0800

    Update localToPdServer.sh
    The first time to sure the ECDSA key

--- Expect/localToPdServer.sh
+++ Expect/localToPdServer.sh
@@ -2,9 +2,19 @@
 set timeout 30
 #port ip
 spawn ssh -l root -p xxxxx xxx.xxx.xxx.xxx
-expect "*password:"
-#password
-send "xxxxxxxxx\r"
+expect {
+    "(yes/no?" {
+            send "yes\n"
+            expect "*password:"
+            #password
+            send "xxxxxxxxx\r"
+    }   
+   "*password:" {
+            #password
+            send "xxxxxxxxx\r"
+   }
+}
+
 expect "~#"
 #ip
 send "ssh -l root xxx.xxx.xxx.xxx\r"
